4ab1264690569d83d571cb810bdca878689cd6b7
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 15:41:21 +0200

    adding REST functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,19 +9,35 @@ import java.util.Map;
 public class App {
     public static void main(String[] args){
         staticFileLocation("/public");
-        get("/", (req,res)->{
+        //get: show new post form
+
+        //post: process new post form
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
             Map<String, Object> model = new HashMap<>();
-            ArrayList<Post> posts = Post.getAll();
-            model.put("posts", posts);
-            return new ModelAndView(model, "index.hbs");
-        },new HandlebarsTemplateEngine());
-//        route that makes a new hero
-        post("/posts/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
+
             String content = request.queryParams("content");
             Post newPost = new Post(content);
             model.put("post", newPost);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get: show all posts
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show an individual post
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
     }
 }
