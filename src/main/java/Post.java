ee2dcd1d94c57806b6326918310356de29a40a2d
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 13:20:05 +0200

    adding a function to avoid content being posted too soon

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -4,5 +4,9 @@ public class Post {
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
     private boolean published;
-
+    public Post(String content){
+        this.content = content;
+        this.published = false;
+        instances.add(this);
+    }
 }
