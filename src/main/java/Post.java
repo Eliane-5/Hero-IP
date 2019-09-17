47fb844d92a27a4831a16d12756a8b6f5f983765
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 13:16:59 +0200

    adding private elements in our class

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Post {
+    private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+
 }
