0dc12bbdf2c3fe98f3a377f15c052c3251a8e8cd
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 14:15:13 +0200

    adding a unique identifier

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -6,11 +6,13 @@ public class Post {
     private static ArrayList<Post> instances = new ArrayList<>();
     private boolean published;
     private LocalDateTime createdAt;
+    private int id;
     public Post(String content){
         this.content = content;
         this.published = false;
         this.createdAt = LocalDateTime.now();
         instances.add(this);
+        this.id = instances.size();
     }
     public String getContent(){
         return content;
@@ -26,5 +28,8 @@ public class Post {
     }
     public LocalDateTime getCreatedAt(){
         return createdAt;
+    }
+    public int getId(){
+        return id;
     }
 }
