996f838ddf9a2bc0c1bc748a980e3c4d28cb2f18
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 14:49:12 +0200

    adding a home route for spark page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,19 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args){
-
+        staticFileLocation("/public");
+        get("/", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+            return new ModelAndView(model, "index.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
