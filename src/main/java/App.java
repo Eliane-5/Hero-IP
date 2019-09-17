cfc8ac9f85fcb9a31135462ddd243c0ea4ab4edf
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-17 20:11:00 +0200

    adding a squad with all hero functionalities

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -71,5 +71,70 @@ public class App {
             deleteHero.deleteHero();
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+//        Squad details in app
+
+
+
+        //get: show new Hero form
+        get("/squads/new", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process new Hero form
+        post("/squads/new", (request, response) -> { //URL to make new hero on POST route
+            Map<String, Object> model = new HashMap<>();
+            String squadName = request.queryParams("squadName");
+            int maxSize = Integer.parseInt(request.queryParams("maxSize"));
+            String cause = request.queryParams("cause");
+            Squad newSquad = new Squad(squadName,maxSize,cause);
+            model.put("squad", newSquad);
+            return new ModelAndView(model, "successSquad.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all Heroes
+        get("/list", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+
+            return new ModelAndView(model, "index2.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show an individual Hero
+        get("/squads/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToFind = Integer.parseInt(req.params(":id")); //pull id - must match route segment
+            Squad foundSquad = Squad.findById(idOfSquadToFind); //use it to find post
+            model.put("squad", foundSquad); //add it to model for template to display
+            return new ModelAndView(model, "squad-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+        //get: show a form to update a Hero
+        get("/squads/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
+            Squad editSquad = Squad.findById(idOfSquadToEdit);
+            model.put("editSquad", editSquad);
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process a form to update a Hero
+        post("/squads/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newSquadName = req.queryParams("squadName");
+            int newMaxSize = Integer.parseInt(req.queryParams("maxSize"));
+            String newCause = req.queryParams("cause");
+            int idOfSquadToEdit = Integer.parseInt(req.params("id"));
+            Squad editSquad= Squad.findById(idOfSquadToEdit);
+            editSquad.update(newSquadName,newMaxSize,newCause); //don’t forget me
+            return new ModelAndView(model, "successSquad.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: delete an individual Hero
+        get("/squads/:id/delete", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfSquadToDelete = Integer.parseInt(req.params("id")); //pull id - must match route segment
+            Squad deleteSquad = Squad.findById(idOfSquadToDelete); //use it to find post
+            deleteSquad.deleteSquad();
+            return new ModelAndView(model, "successSquad.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,3 +1,57 @@
-public class Squad {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-}
+public class Squad {
+        private String squadName;
+        private int maxSize;
+        private String cause;
+        private static ArrayList<Squad> instances = new ArrayList<Squad>();
+        private boolean published;
+        private LocalDateTime createdAt;
+        private int id;
+        public Squad(String squadName, int maxSize, String cause){
+            this.squadName = squadName;
+            this.maxSize = maxSize;
+            this.cause = cause;
+            this.published = false;
+            this.createdAt = LocalDateTime.now();
+            instances.add(this);
+            this.id = instances.size();
+        }
+        public String getSquadName(){
+            return squadName;
+        }
+        public int getMaxSize(){
+            return maxSize;
+        }
+        public String getCause(){
+            return cause;
+        }
+        public static ArrayList<Squad> getAll(){
+            return instances;
+        }
+        public static void clearAllHeroes(){
+            instances.clear();
+        }
+        public boolean getPublished(){
+            return this.published;
+        }
+        public LocalDateTime getCreatedAt(){
+            return createdAt;
+        }
+        public int getId(){
+            return id;
+        }
+        public static Squad findById(int id){
+            return instances.get(id-1);
+        }
+        public void update(String newSquadName, int newMaxSize, String newCause){
+            this.squadName = newSquadName;
+            this.maxSize = newMaxSize;
+            this.cause = newCause;
+        }
+        public void deleteSquad(){
+            instances.remove(id-1);
+        }
+    }
+
